43e4a8dbe86a943faccbcea90b2920992548bad6
liuenbo <liuenbofuture@foxmail.com>
2015-02-18 21:16:48 +0800

    实现了用express跟相关中间件的session的数据库存储的模块引入
    必须使用 resave: true,saveUninitialized: true来消除警告

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require('express');//PS：4.x将中间件分离了出来，所以如果想要使用哪个模块就需要自己手动npm install
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//import sessions Persistence modules
+var flash = require('connect-flash');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var setting = require('./setting');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,12 +19,27 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//show use flash modules
+app.use(flash());
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());//app.use(express.cookieParser()); 3.x
+//引入session连接mongodb
+app.use(session({
+    secret: setting.cookieSecret,
+    proxy: true,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+        db: setting.db
+    }, function() {
+        console.log('connect mongodb success...');
+    })
+}));
+
 app.use(    express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
