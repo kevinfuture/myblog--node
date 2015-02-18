5c98fdd573ffbdfe4707c7a893fe6f0aec0accb9
liuenbo <liuenbofuture@foxmail.com>
2015-02-18 16:13:07 +0800

    创建连接

---
+++ models/db.js
@@ -0,0 +1,16 @@
+/**
+ * Created by kevin on 2015/2/18/0018.
+ */
+
+
+//导入要连接的模块文件setting.js
+var setting = require("../setting");
+//获取数据对象
+var Db = require("mongodb").Db();
+//常规来说的创建连接
+var Connections = require("mongodb").Connection;
+//获取服务对象
+var Server = require("mongodb").Server;
+//创建连接并且将接口exports出来
+var exports = new Db(setting.db,new Server(setting.host,Connections.DEFAULT_PORT,{}),{safe:true});
+
