0350d1956b2280c4134e207129d86c8af29be486
liuenbo <liuenbofuture@foxmail.com>
2015-02-26 19:14:54 +0800

    条理化代码，总是签出最新分支

--- models/BaseObject.js
+++ models/BaseObject.js
@@ -1,12 +1,11 @@
 var db = require('../models/db');
 var Schema = db.mongoose.Schema;
-var ObjectId =Schema.Types.ObjectId;//注意写法，大小写要正确，最好使用webstorm的自动补全，这样可以定位到相应的模块代码
+var ObjectId =Schema.Types.ObjectId;
 
-var BaseObjecSchema = new Schema({
-    // _id:ObjectId,默认有_id所以我不用创建_id，如果要用_id的话可以使用索引index：true，有种写法是这样的但是没有成功id:{type:ObjectId,default:function(){return new ObjectId;}}
-    IsDelete:{type:Boolean,default:false},
-    //Version:String,  在mongodb中已经自动创建了_v所以此处省略了
-    Entity:{type:String,default:'Entity'},//没有用过
-    Date:{type:Date,default:Date.now()}
+var BaseObjecSchema = new Schema({//在mongodb中会按照相反的顺序排列
+    Date:{type:Date,default:Date.now()},
+    Entity:{type:Number,default:0},
+    Version:{type:Number,default:1},
+    IsDelete:{type:Boolean,default:false}
 } ,{ _id: false, autoIndex: false });//禁止自动创建_id跟索引
 exports.BaseObject = db.mongoose.model('BaseObject',BaseObjecSchema) ;
--- models/User.js
+++ models/User.js
@@ -5,13 +5,11 @@
 var db = require('../models/db');
 var baseObject = require('../models/BaseObject');
 var Schema = db.mongoose.Schema;
-var ObjectId =Schema.Types.ObjectId;//注意写法，大小写要正确，最好使用webstorm的自动补全，这样可以定位到相应的模块代码
+var ObjectId =Schema.Types.ObjectId;
 var UserSchema = new Schema({
-    //id:{type:ObjectId,default:null},//此处的id并不是mongodb中的“_id”，是自定义的id，其中设置ObjectId有困难,但是可以取出_id
     name:{type:String,index: true},//此处我设置了name为唯一性的索引，它自动指向数据文档的_id，所以我可以不用设置_id
     password:String,
-    baseObj:{type:Array,default:baseObject.BaseObject.call(this)}//这样就可以直接从BaseObject获取基本数据了，只是这种方法还有待改善，这不是继承上层的东西
-    //当然还有一种是用中间件的方法（mongoose-schema-extend）参考网址 ：http://my.oschina.net/antianlu/blog/187316
+    baseObj:{type:Array,default:baseObject.BaseObject.call(this)}
 });
 UserSchema.methods.find = function (username, callback) {
    return this.find({ username: username }, callback);
@@ -36,15 +34,4 @@ UserSchema.static('findOrCreate', function (query, cb) {
         else cb(null, doc)
     })
 })
-//UserSchema.pre('save',function(user,callback){
-    //var user = {
-    //    name : this.name,
-    //    //email : this.email,
-    //    password : this.password
-    //};
-    //this.insert(user,{safe:true},function(err){//有错误
-    //    callback();
-    //});
-
-//})
 exports.User = db.mongoose.model('User',UserSchema) ;
