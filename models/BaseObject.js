ef0c028883531c8d0f0c36d45c320b7ac1c6a9cc
liuenbo <liuenbofuture@foxmail.com>
2015-02-26 19:05:21 +0800

    完成了基本数据BaseObject的复用，但是根据冯天然来说还有进一步的继承来实现会更好

--- models/BaseObject.js
+++ models/BaseObject.js
@@ -1,63 +1,12 @@
-/**
- * Created by kevin on 2015/2/20/0020.
- */
-var mongodb = require('./db');
+var db = require('../models/db');
+var Schema = db.mongoose.Schema;
+var ObjectId =Schema.Types.ObjectId;//注意写法，大小写要正确，最好使用webstorm的自动补全，这样可以定位到相应的模块代码
 
-//注意这里还有错误，因为_id是ObjectId的类型，所以要封装一下
-
-function BaseObject(baseObject){
-    this.IsDelete = baseObject.IsDelete;
-    this.Version = baseObject.Version;
-    this.Entity = baseObject.Entity;
-    this.IsDelete=function(){
-        this.IsDelete=false;
-    }
-}
-BaseObject.get = function(username,callback){
-    mongodb.open(function(err,db){
-        if(err){
-            return callback(err);
-        }
-        db.collection('BaseObject',function(err,collection){
-            if(err){
-                mongodb.close();
-                return callback(err);
-            }
-            collection.findOne({_id:uid},function(err,doc){
-                mongodb.close();
-                if(doc){
-                    var baseObject = new BaseObject(doc);
-                    callback(err,user);
-                }else{
-                    callback(err,null);
-                }
-            });
-        })
-    });
-}
-
-module.exports = BaseObject;
-
-User.prototype.save = function save(callback){
-    var baseObject = {
-        IsDelete : this.IsDelete,
-        Version : this.Version,
-        Entity : this.Entity
-    };
-    mongodb.open(function(err,db){
-        if(err){
-            return callback(err);
-        }
-        db.collection('BaseObject',function(err,collection){
-            if(err){
-                mongodb.close()
-                return callback(err);
-            }
-            collection.ensureIndex('_id',{unique:true});//mongodb支持索引，所以在这里给name设置所以可以提高查询速度
-            collection.insert(baseObject,{safe:true},function(err){
-                mongodb.close();
-                callback();
-            });
-        })
-    });
-}
+var BaseObjecSchema = new Schema({
+    // _id:ObjectId,默认有_id所以我不用创建_id，如果要用_id的话可以使用索引index：true，有种写法是这样的但是没有成功id:{type:ObjectId,default:function(){return new ObjectId;}}
+    IsDelete:{type:Boolean,default:false},
+    //Version:String,  在mongodb中已经自动创建了_v所以此处省略了
+    Entity:{type:String,default:'Entity'},//没有用过
+    Date:{type:Date,default:Date.now()}
+} ,{ _id: false, autoIndex: false });//禁止自动创建_id跟索引
+exports.BaseObject = db.mongoose.model('BaseObject',BaseObjecSchema) ;
