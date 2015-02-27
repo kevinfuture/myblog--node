a492468c143901a41a27bfd58a1f6b536902aed6
liuenbo <liuenbofuture@foxmail.com>
2015-02-27 18:17:40 +0800

    这里的注释很重要

--- routes/BlogController/index.js
+++ routes/BlogController/index.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 /* GET home page. */
 router.get('/Blog/index', function(req, res, next) {
     res.render('Blog/index', { title: 'Blog' });//这个地方用 'Blog/index','/Blog/index'
+    //*****这个的是针对域名的，但是app.js中路由的渲染是对文件的*****
 });
 
 module.exports = router;
