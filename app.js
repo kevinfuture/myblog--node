4b7dca425ce231c74ae287d1a904554e2722417b
liuenbo <liuenbofuture@foxmail.com>
2015-03-02 10:11:28 +0800

    完成了新随笔页面的ueditor的文本编辑器的功能，继续优化

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//引入模块
+var ueditor = require("ueditor");
 
 var setting = require('./setting');
 //import sessions Persistence modules
@@ -106,6 +108,33 @@ app.get('/Album/photo',photo);
 app.get('/File/uploadfile',uploadfile);
 
 
+//ueditor的图片存放路径的路由代码
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+        var date = new Date();
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/images/ueditor/';
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/images/ueditor/';
+        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+    }
+    // 客户端发起其它请求
+    else {
+        // console.log('config.json')
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/ueditor/nodejs/config.json');
+    }
+}));
+app.use('/', function(req, res) {
+    res.render('ueditor');
+});
+//到这里结束
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
