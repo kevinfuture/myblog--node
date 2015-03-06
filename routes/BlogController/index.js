b72071b7b0103cacd64243afa256b72b34d79270
liuenbo <liuenbofuture@foxmail.com>
2015-03-06 09:33:25 +0800

    这块注释掉了，需要重新考虑，放到最后

--- routes/BlogController/index.js
+++ routes/BlogController/index.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 var moment = require('moment');
 var User = require('../../models/User.js');
 var EssayPost = require('../../models/EssayPost');
+var EventProxy = require('./eventproxy');
+var proxy = new EventProxy();
 
 /* GET home page. */
 router.get('/:users', function(req, res, next) {
@@ -20,6 +22,21 @@ router.get('/:users', function(req, res, next) {
         mongoose.model('EssayPost').count({username: name}, function (err, essaycount) {//这里返回的是总的文章个数
             mongoose.model('EssayPost').find({username: name}, function (err, essaylist) {//这里返回的是对象
                 //***************注意以后这里要做分页********************
+                //var time = new Date();
+                //time.setMonth(time.getMonth()+1);
+                //time.setDate(0);
+                //time.getDate();
+                //var calenderarray=[];
+                //var ep = EventProxy.create("template", "data", "l10n", function (template, data, l10n) {
+                //    _.template(template, data, l10n);
+                //});
+                //for(var i=0;i<time.getDate();i++){
+                //    mongoose.model('EssayPost').count({username: name,TDate:3+'/'+i+'/'+2015},function(err,objcount){
+                //        objcount!=0?calenderarray.unshift(objcount):calenderarray.unshift(0);
+                //        ep.emit(calenderarray.count, calenderarraycount);
+                //    })
+                //    console.log('####：' + calenderarray);
+                //}
                 res.render('Blog/index', {
                     title: name,
                     user: user,
