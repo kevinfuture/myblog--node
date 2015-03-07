46def8cc9d4c1bfae91edd5c2c75b4b9103cdf80
liuenbo <liuenbofuture@foxmail.com>
2015-03-07 20:49:51 +0800

    修改

--- routes/BlogController/Essay/newessay.js
+++ routes/BlogController/Essay/newessay.js
@@ -20,48 +20,60 @@ router.get('/Essay/newessay/:_id?', function(req, res, next) {
 });
 
 router.post('/Essay/newessay', function(req, res, next) {
-    mongoose.model('EssayPost').count({username:req.session.user.name,caption:req.body.caption},function(err,essay) {
-        mongoose.model('EssayPost').count({username: req.session.user.name}, function (err, essaycount) {//这里返回的是总的文章个数
-            mongoose.model('EssayPost').find({username:  req.session.user.name}, function (err, essaylist) {//这里返回的是对象
-                if (essay == 0) {
-                    var newEssayPost = new EssayPost.EssayPost({
-                        username: req.session.user.name,
-                        caption: req.body.caption,
-                        content: req.body.content,
-                        TDate:moment(Date.now()).format('l')
-                    });
-                    newEssayPost.save(function (err) {
-                        if (err) {
-                            req.flash('error', err);
-                            return res.redirect('/Essay/newessay');
-                        }
-                        req.flash('success', '创建新随笔成功！！！');
-                        res.render('Blog/index', {
-                            title: req.session.user.name,
-                            essaycount: essaycount == 0 ? 0 : essaycount,//文章数目
-                            essaylist:essaylist//文章的对象
+    mongoose.model('User').findOne({name:req.session.user.name},function(err,user) {
+        var age = moment(moment(user.baseObj[0].Date).format('YYYY/MM/DD'), 'YYYYMMDD').fromNow().split(' ');
+        user.baseObj[0].Date = age[0] + ' ' + age[1];
+        mongoose.model('EssayPost').count({
+            username: req.session.user.name,
+            caption: req.body.caption
+        }, function (err, essay) {
+            mongoose.model('EssayPost').count({username: req.session.user.name}, function (err, essaycount) {//这里返回的是总的文章个数
+                mongoose.model('EssayPost').find({username: req.session.user.name}, function (err, essaylist) {//这里返回的是对象
+                    console.log('获取id测试：' + req.body.essay_id);
+                    if (essay == 0) {
+                        var newEssayPost = new EssayPost.EssayPost({
+                            username: req.session.user.name,
+                            caption: req.body.caption,
+                            content: req.body.content,
+                            TDate: moment(Date.now()).format('l')
                         });
-                    });
-                } else {
-                    mongoose.model('EssayPost').findOneAndUpdate({
-                        username: req.session.user.name,
-                        caption: req.body.caption
-                    }, {caption: req.body.caption, content: req.body.content,TDate:moment(Date.now()).format('l')}, function (err, essay) {
-                        if (err) {
-                            req.flash('error', err);
-                            return res.render('Blog/Essay/newessay', {
-                                title: req.session.user.name + '的随笔',
-                                caption: essay == null ? null : essay.caption,
-                                content: essay == null ? null : essay.content
+                        newEssayPost.save(function (err) {
+                            if (err) {
+                                req.flash('error', err);
+                                return res.redirect('/Essay/newessay');
+                            }
+                            req.flash('success', '创建新随笔成功！！！');
+                            res.render('Blog/index', {
+                                user:user,
+                                essaycount: essaycount == 0 ? 0 : essaycount,//文章数目
+                                essaylist: essaylist//文章的对象
                             });
-                        }
-                        return res.render('Blog/index', {
-                            title: req.session.user.name,
-                            essaycount: essaycount == 0 ? 0 : essaycount,//文章数目
-                            essaylist:essaylist//文章的对象
                         });
-                    });
-                }
+                    } else {
+                        mongoose.model('EssayPost').findOneAndUpdate({
+                            username: req.session.user.name,
+                            caption: req.body.caption
+                        }, {
+                            caption: req.body.caption,
+                            content: req.body.content,
+                            TDate: moment(Date.now()).format('l')
+                        }, function (err, essay) {
+                            if (err) {
+                                req.flash('error', err);
+                                return res.render('Blog/Essay/newessay', {
+                                    title: req.session.user.name + '的随笔',
+                                    caption: essay == null ? null : essay.caption,
+                                    content: essay == null ? null : essay.content
+                                });
+                            }
+                            return res.render('Blog/index', {
+                                user:user,
+                                essaycount: essaycount == 0 ? 0 : essaycount,//文章数目
+                                essaylist: essaylist//文章的对象
+                            });
+                        });
+                    }
+                });
             });
         });
     });
