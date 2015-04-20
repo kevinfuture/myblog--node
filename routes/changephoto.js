8a653ae484996b7ccf354318bf77bdfac02ef876
liuenbo <liuenbofuture@foxmail.com>
2015-04-20 11:18:03 +0800

    头像更换代码

--- routes/changephoto.js
+++ routes/changephoto.js
@@ -25,44 +25,55 @@ router.get('/changephoto', function(req, res) {
 });
 router.post('/changephoto', isLogin.checkLogin);
 router.post('/changephoto', function(req, res, next) {
-    console.log('#######'+req.files.headPic);
-    console.log('*************'+req.body.headPic);
     var pic = req.files.headPic,
         temtype,
         mat = pic.type.match(/(gif|jpeg|png|jpg)/g),
         originalpath = pic.path,
-        newpath = setting.uploadDir+'headPic'+req.session.user.name+'.';
+        newpath = '../../mycode/blog/public/'+setting.uploadDir+'headPic'+req.session.user.name+'.';
+    var target = new Array();
     var ImageType = {
         'jpeg':'jpg',
         'jpg':'jpg',
         'gif':'gif',
         'png':'png'
     };
-    if(!mat){
-        req.flash('error', '（︶︿︶） 图片格式不对吧亲~');
-        fs.unlink(originalpath);
-        return res.redirect('/changephoto');
-    }else if(pic.length > 204800){
-        req.flash('error','（︶︿︶） 图片太大了，服务器表示鸭梨好大');
-        fs.unlink(originalpath);
-        return res.redirect('/changephoto');
-    }else if(pic.length==undefined){
+    if(pic.size==0){
         req.flash('error','（︶︿︶） 亲，还没有选择图片吧！');
+        console.log("还没有选择图片吧");
+        fs.unlink(originalpath);
+        return res.redirect('/changephoto');
+    }else if(!mat){
+        req.flash('error', '（︶︿︶） 图片格式不对吧亲~');
+        console.log("图片格式不对");
+        fs.unlink(originalpath);
+        return res.redirect('/changephoto');
+    }else if(pic.size > 204800){
+        req.flash('error','（︶︿︶） 图片太大了，服务器表示鸭梨好大');
+        console.log("图片太大了");
         fs.unlink(originalpath);
         return res.redirect('/changephoto');
     }
     temtype = ImageType[mat[0]];
-    console.log("*************************************");
-    fs.rename(originalpath,newpath+=temtype,function(err){
-        console.log("#####################"+req.session.user._id);
-        mongoose.model('User').findOneAndUpdate({_id:req.session.user._id},{headPic:newpath}, function(err, user) {
-            if (err) {
-                req.flash('error', err);
-                return res.redirect('/changephoto');
-            }
-            res.redirect('/users');
-        });
+    newpath+=temtype;
+    var path = 'D:/NodeProject/mycode/blog/public'+newpath;
+    fs.unlink(newpath,function(deleteerr){
+        fs.rename(originalpath,newpath,function(err){
+            console.log("#####################"+req.session.user._id);
+            mongoose.model('User').findOneAndUpdate({_id:req.session.user._id},{headPic:newpath}, function(err, user) {
+                if(deleteerr)
+                {console.log(deleteerr);}
+                if (err) {
+                    req.flash('error', err);
+                    return res.redirect('/changephoto');
+                }
+                res.redirect('/users');
+            });
+        })
+
+        console.log('ddddd');
     })
+    console.log('$$$$$$$$$$$$'+newpath);
+
 });
 
 module.exports = router;
