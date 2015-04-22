bf4d6c9cce2a95fa09e7974eb010700f49c81b27
xieyanxia <1457682643@qq.com>
2015-04-22 09:22:43 +0800

    侧边栏样式调整

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -1,21 +1,34 @@
 /**
  * Created by Administrator on 2015/3/9.
  */
+
 $(function(){
+    var bShow = false;
     $(".nav #base-msg").click(function(){
-        $('.nav #base-msg .hide').toggle();
+        bShow = true;
+        $('.nav #base-msg .hide').show();
     });
+    //$(document).click(function(){
+    //    if(bShow == true){
+    //        $('.nav #base-msg .hide').hide();
+    //    }
+    //});
+
 });
 $(function(){
     var iNow = 0;
-
     var timer = null;
+    var oUL = $('.bd ul').html() +   $('.bd ul').html();
+    var nUl = $('.bd ul').html( oUL);
+    console.log(nUl);
     function bb(){
         if(iNow == $('.bd li').length-1){
+            $('.bd ul').animate({"left":-480*(iNow)},700);
             iNow = 0;
         }else{
             iNow++;
         }
+
         $('.bd ul').animate({"left":-480*(iNow)},700);
         //jQuery(".b_left .hoverBg").animate({"margin-top":60*iNow+"px"},700);
     }
@@ -36,19 +49,9 @@ $(function(){
       clearInterval(timer);
     });
     $('.bd').mouseout(function(){
-        timer = setInterval(bb,2000);
+        timer = setInterval(bb,3000);
     });
-    timer = setInterval(bb,2000);
+    timer = setInterval(bb,3000);
 });
 
-//// 正则表达式--用户名
-//var reg=/^[a-z0-9-]{6,10}$/;
 
-//// 正则表达式--密码
-//var reg=/^[a-z0-9]{6,10}$/;
-
-//// 正则表达式--手机号
-//var reg=/^1[3|5|8]\d{9}$/;
-
-//// 正则表达式--邮箱
-//var reg=/^\w+@[a-z0-9-]+(\.[a-z]{2,6}){1,2}$/;
