da74bce71d268765086530456ab1125e43b0f49c
liuenbo <liuenbofuture@foxmail.com>
2015-04-22 14:31:36 +0800

    小修改

--- routes/BlogController/Essay/commentDel.js
+++ routes/BlogController/Essay/commentDel.js
@@ -9,13 +9,16 @@ module.exports = function(router) {
     router.get('/commentDel', function (req, res) {
         var commentid = req.query.commentid,
             essayUsername = req.query.essayUsername,
-            essayCaption = req.query.essayUsername;
+            essayCaption = req.query.essayCaption;
         if (!req.session.user) {
             req.flash('error', '未登入');
             return res.redirect('/login');
         }
         mongoose.model('Comment').remove({_id: commentid}, function (delComerr, delCommentById) {
+            console.log('##########'+essayUsername);
+            console.log('**************'+essayCaption);
             mongoose.model('EssayPost').findOne({username: essayUsername,caption:essayCaption}, function (essayErr, findIdByUserCaption) {
+                console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'+findIdByUserCaption);
                 if (delComerr) {
                     req.flash('error', delComerr);
                     return res.redirect('/Essay/showessay/' + findIdByUserCaption._id);
