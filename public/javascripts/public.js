646835fa331785b57d745e5442675808c389e96b
xieyanxia <1457682643@qq.com>
2015-04-22 11:22:47 +0800

    样式调整

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -3,47 +3,37 @@
  */
 
 $(function(){
-    var bShow = false;
-    $(".nav #base-msg").click(function(){
-        bShow = true;
+    $(".nav #base-msg").mouseover(function(){
         $('.nav #base-msg .hide').show();
     });
-    //$(document).click(function(){
-    //    if(bShow == true){
-    //        $('.nav #base-msg .hide').hide();
-    //    }
-    //});
-
+    $(".nav #base-msg").mouseout(function(){
+        $('.nav #base-msg .hide').hide();
+    });
 });
 $(function(){
     var iNow = 0;
     var timer = null;
-    var oUL = $('.bd ul').html() +   $('.bd ul').html();
-    var nUl = $('.bd ul').html( oUL);
-    console.log(nUl);
+    var aLi = $('.bd ul li');
+    $('.bd ul').css({'width':(aLi.length *aLi.eq(0).width() )+'px'});
     function bb(){
-        if(iNow == $('.bd li').length-1){
-            $('.bd ul').animate({"left":-480*(iNow)},700);
-            iNow = 0;
-        }else{
-            iNow++;
+        iNow++;
+        if(iNow ==  (aLi.length)){
+            $('.bd ul').css({'left':0 +'px'});
+            iNow =1;
         }
-
         $('.bd ul').animate({"left":-480*(iNow)},700);
-        //jQuery(".b_left .hoverBg").animate({"margin-top":60*iNow+"px"},700);
     }
-    //bb();
     $('#next').click(function(){
         bb();
     });
     $('#pre').click(function(){
         iNow--;
         if(iNow ==  -1){
-            iNow =$('.bd li').length-1;
+            $('.bd ul').css({'left':-(($('.bd li').length-1) *aLi.eq(0).width()) +'px'});
+            iNow =$('.bd li').length-2;
         }
 
         $('.bd ul').animate({"left":-480*(iNow)},700);
-        //jQuery(".b_left .hoverBg").animate({"margin-top":60*iNow+"px"},700);
     });
     $('.bd').mouseover(function(){
       clearInterval(timer);
